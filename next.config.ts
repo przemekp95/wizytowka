19874903ca6de3255356293deb8b44fd2c64e5fe
Przemysław Pietrzak <przemekp95@wp.pl>
2025-09-19 00:31:41 +0200

    Ujednolicone .gitignore w root, usunięto lokalne gitignore w backend/frontend

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  output: 'standalone', // ← ten tryb buduje server.js + minimalne node_modules
-  images: { unoptimized: true }, // opcjonalne, jeśli nie chcesz loadera obrazków
-};
-
-export default nextConfig;
