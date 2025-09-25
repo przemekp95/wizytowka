3ada41244b7e6270fafa36dc23be8ee1e7ddb028
Przemysław Pietrzak <przemekp95@wp.pl>
2025-09-25 14:14:38 +0200

    Fix

--- backend/src/portfolio/portfolio.module.ts
+++ backend/src/portfolio/portfolio.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioApiController } from './portfolio.controller';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
+  imports: [AwsModule],
   providers: [PortfolioService],
   controllers: [PortfolioApiController],
   exports: [PortfolioService],
