c02e21cc7ed62d72f6c120d1f98e37955edd6c92
Przemysław Pietrzak <przemekp95@wp.pl>
2025-10-27 13:24:59 +0100

    fix: add jest setup file to mock ES6 modules in E2E tests

---
+++ backend/test/test-setup.ts
@@ -0,0 +1,26 @@
+// Mock problematic ES6 modules for Jest E2E tests
+// @ts-ignore
+jest.mock('uuid', () => ({
+  // @ts-ignore
+  v4: jest.fn(() => 'mocked-uuid-1234-5678-9abc-def012345678')
+}));
+
+// @ts-ignore
+jest.mock('@aws-sdk/client-s3', () => ({
+  // @ts-ignore
+  S3Client: jest.fn().mockImplementation(() => ({
+    // @ts-ignore
+    send: jest.fn()
+  })),
+  // @ts-ignore
+  DeleteObjectCommand: jest.fn()
+}));
+
+// @ts-ignore
+jest.mock('@aws-sdk/lib-storage', () => ({
+  // @ts-ignore
+  Upload: jest.fn().mockImplementation(() => ({
+    // @ts-ignore
+    done: jest.fn().mockResolvedValue({ Location: 'mocked-s3-url' })
+  }))
+}));
