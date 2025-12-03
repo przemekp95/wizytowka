169893914076d74cb973974a52c35a4a00da1b27
Przemysław Pietrzak <przemekp95@wp.pl>
2025-12-03 01:33:43 +0100

    re-enable ContactMessageInput validation - test if this was blocking

--- backend/src/contact/dto/contact-message.input.ts
+++ backend/src/contact/dto/contact-message.input.ts
@@ -1,25 +1,33 @@
 import { Field, InputType } from '@nestjs/graphql';
-// TEMP: Disabled validation for testing
-// import {
-//   IsEmail,
-//   IsOptional,
-//   IsString,
-//   MaxLength,
-//   MinLength,
-// } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 @InputType()
 export class ContactMessageInput {
   @Field()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   name!: string;
 
   @Field()
+  @IsEmail()
   email!: string;
 
   @Field()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(2000)
   message!: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   hcaptchaToken?: string;
 }
 
